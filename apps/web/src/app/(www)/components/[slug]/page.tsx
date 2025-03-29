c3d02afa3881b4a0e4fbf583215e39cb4cba3a48
Lucas Roig <lucas1.roig@gmail.com>
2025-03-29 17:31:52 +0100

    feat(component page): add some styling to the mdx renderer

--- apps/web/src/app/(www)/components/[slug]/page.tsx
+++ apps/web/src/app/(www)/components/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { MDXContent } from "@content-collections/mdx/react";
 import { allComponents } from "content-collections";
+import { Title } from "../../../../@components/typography";
 import { ComponentPreview } from "../_components/component-preview";
 
 export default async function ComponentPage({ params }: Readonly<{ params: Promise<{ slug: string }> }>) {
@@ -10,11 +11,27 @@ export default async function ComponentPage({ params }: Readonly<{ params: Promi
   }
   return (
     <div>
-      This is a component page {slug}
       <MDXContent
         code={component.mdx}
         components={{
-          ComponentPreview,
+          ComponentPreview: ({ children }: { children: React.ReactNode }) => (
+            <div className="mb-6">
+              <ComponentPreview>{children}</ComponentPreview>
+            </div>
+          ),
+          h1: ({ children }: { children: React.ReactNode }) => <Title.H1 className="mb-8  ">{children}</Title.H1>,
+          h2: ({ children }: { children: React.ReactNode }) => <Title.H2 className="mb-6">{children}</Title.H2>,
+          p: ({ children }: { children: React.ReactNode }) => <p className="mb-6">{children}</p>,
+          ul: ({ children }: { children: React.ReactNode }) => (
+            <ul
+              className={`
+                pl-4 mb-6 [&_ul]:mb-0
+                list-disc [&_ul]:list-[circle] [&_ul_ul]:list-[square]
+              `}
+            >
+              {children}
+            </ul>
+          ),
         }}
       />
     </div>
